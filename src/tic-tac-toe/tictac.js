46b9689e313001c50a5c40384a261108069eaded
David Bille <d_bille@yahoo.com>
2019-07-21 21:23:14 -0500

    jQuery

--- src/tic-tac-toe/tictac.js
+++ src/tic-tac-toe/tictac.js
@@ -1,34 +1,13 @@
-//previous click 'odd or even'
-//var player1, var player2, var previousClick
 
-// console.log('hey')
-// var box = document.querySelector('.box')
-// var allBoxes = document.querySelectorAll('.box')
-// box.onclick = function setXO (event) {
-//   console.log('hey you clicked me', event)
-//   event.target.innerText = '0'
-// }
-var gamemarker = 'x'
-// function changeMarkerToX(){ }
-// function changeMarkerToO(){ }
+
+var gamemarker = 'X'
 
  function playTicTacToe(element) {
-  //  if(element.innerText === ('x' || 'o')) {
-  //    alert('no')
-  //    return
-  //  }
-   if (gamemarker === 'x') {
-     element.innerText = 'o'
-     gamemarker = 'o'
-     
-   } else {
-    element.innerText = 'x'
-    gamemarker = 'x'
-     
+     if (gamemarker === 'X') {
+     element.innerText = 'O'
+     gamemarker = 'O'
+    } else {
+    element.innerText = 'X'
+    gamemarker = 'X'
    }
  }
-// if () {
-  
-// } else {
-  
-// }
