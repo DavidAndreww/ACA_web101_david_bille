6829ec2da3a4a7f7455b1ce5d2cf6571795a65ab
David Bille <d_bille@yahoo.com>
2019-07-27 12:27:48 -0500

    finished

--- src/TowersOfHanoi/tower.js
+++ src/TowersOfHanoi/tower.js
@@ -1,36 +1,50 @@
-/*
+
 'use strict';
 
 $(document).ready(function() {
-  let $block = null;
-  
+  let heldblock = null;
+
+
   $('.data-row').click(function() {
-    if ($block) {
-      $(this).prepend($block);
-      $block = null;
+    if (heldblock) {
+      if (heldblock.data('value') < $(this).children().first().data('value')||
+      $(this).children().length == 0) {
+        $(this).prepend(heldblock);
+      heldblock = null;
+      }  
+
+      $(this).prepend(block);
+      heldblock = null;
     } else {
-      $block = $(this).children().first().detach();
+      heldblock = $(this).children().first().detach();
+    console.log(heldblock.data('value'));
     }
+    
+    let grabbedblock = null;
+
+
+
+    // let firstdisc = $('[data-value]:first-child')
+    // console.log($(this));
+    // if (block.data('value') >) {
+       
+    // } else {
+      
+    // }
   })
 })
-*/
 
-'use strict';
+// INSTRUCTIONS //
+//1: 3 discs start on gray peg
+//2: Ordered from smallest to largest disc
+//3: Move discs to green peg
+//4: Can only move 1 disc at a time
+//5: Value of top disc < value of disc below it
+//6: Value of bottom disc > value of disc above it
 
-function stepsToSolveHanoiT(height, srcP, desP, bufferP) {
-  if (height >= 1) {
+// OBJECTIVES //
+//1: Cursor is empty, next click will detach child fom peg
+//2: Cursor has child, next click will attach child to peg
+//3: If bottom disc is greater value, can detach
+//3b: else, disc cannot detach
 
-    // Move a tower of height-1 to the buffer peg, using the destination peg.
-    stepsToSolveHanoiT(height - 1, srcP, bufferP, desP);
-
-    // Move the remaining disk to the destination peg.
-    console.log('Move disk from Tower ', srcP, ' to Tower ', desP);
-
-    // Move the tower of `height-1` from the `buffer peg` to the `destination peg` using the `source peg`.        
-    stepsToSolveHanoiT(height - 1, bufferP, desP, srcP);
-  }
-  
-  return;
-}
-
-stepsToSolveHanoiT(3, "A", "C", "B");
